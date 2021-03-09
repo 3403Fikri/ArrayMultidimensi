a0639994f6f9601a8c2bd327bb5b09b247e5f5a8
Muhammad Fikri Ramadhan <fikri@students,amikom.ac.id>
2021-03-09 17:47:22 +0700

    Fix Application not working because of poor for application

--- Array.cpp
+++ Array.cpp
@@ -8,7 +8,7 @@ int main()
 
     for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             cout << "Masukan angka baris ke - " << i + 1 << " Kolom ke - " << j + 1 << ": ";
             cin >> a[i][j];
@@ -18,9 +18,10 @@ int main()
 
     for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             cout << "angka baris ke - " << i + 1 << " kolom ke - " << j + 1 << ": " << a[i][j];
+            cout << endl;
         }
         cout << endl;
     }
