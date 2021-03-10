4497c11f3e57347ff6ce1f0cdaa3400ba9a31c72
Muhammad Fikri Ramadhan <fikri@students,amikom.ac.id>
2021-03-10 20:07:38 +0700

    Menggabungkan code agar menjalankan semua fungsi

--- Array.cpp
+++ Array.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+void latihanarray()
 {
     int a[2][3];
 
@@ -25,4 +25,93 @@ int main()
         }
         cout << endl;
     }
+}
+
+void buatSilang()
+{
+    cout << "Masukan ordo array: ";
+    int banyak;
+    cin >> banyak;
+
+    for (int x = 0; x < banyak; x++)
+    {
+        for (int y = 0; y < banyak; y++)
+        {
+            if (x == y || y == (banyak - x - 1))
+            {
+                cout << " * ";
+            }
+            else
+            {
+                cout << "   ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void tambahmatrik()
+{
+    int matrixa[3][3], matrixb[3][3], total[3][3];
+
+    cout << "masukan angka matrix a" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Masukan baris ke " << i + 1 << " kolom ke " << j + 1 << ": ";
+            cin >> matrixa[i][j];
+        }
+    }
+    cout << endl;
+    cout << "masukan angka matrix b" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Masukan baris ke " << i + 1 << " kolom ke " << j + 1 << ": ";
+            cin >> matrixb[i][j];
+        }
+    }
+
+    cout << endl;
+
+    cout << "Matrix a" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+
+            cout << matrixb[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Matrix B" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+
+            cout << matrixb[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Total : " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            total[i][j] = matrixa[i][j] + matrixb[i][j];
+            cout << total[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    
+    cin.get();
+    return 0;
 }
